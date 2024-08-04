b87e33af93a024c06b8c85b7f4e58e137c205b2a
Richard Pierre <98787241+Tom-RL@users.noreply.github.com>
2024-08-04 20:22:11 -0300

    Aprendendo sobre a instrução Switch

---
+++ Teste/switch.cs
@@ -0,0 +1,24 @@
+﻿/*
+int employeeLevel = 200; // Nível do funcionário
+string employeeName = "John Smith"; // Nome do Funcipnário
+
+string title = ""; // Título que é dado ao Funcionário
+
+switch (employeeLevel) // Verifica qual título vai ser dado ao Funcionário com a instrução SWITCH!
+{
+    case 100:
+    case 200:
+        title = "Senior Associate";
+        break;
+    case 300:
+        title = "Manager";
+        break;
+    case 400:
+        title = "Senior Manager";
+        break;
+    default:
+        title = "Associate";
+        break;
+}
+
+Console.WriteLine($"{employeeName}, {title}"); // Mostra na saída como ficará o nome e o nível do Funcionário */
